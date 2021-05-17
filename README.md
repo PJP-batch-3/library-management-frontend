b11f26c374d2579a009e1d3c88aa1f9c8cb136e3
Deepanjan Datta <deepanjan052000@gmail.com>
2021-05-18 03:37:31 +0530

    Added static pages
    Added static landing page, login page and template file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # library-management-frontend
+
+## Instructions
+Use template.html as the base html file for any page you design. The content in template.html should not be changed in order to maintain consistency.
+
+For bootstrap reference:
+- https://demos.creative-tim.com/material-kit/docs/2.1/getting-started/introduction.html
+- https://getbootstrap.com/docs/4.0/getting-started/introduction/
---
+++ src/base.js
@@ -0,0 +1,45 @@
+var baseUrl = 'http://localhost:8080/api/';
+var error_msg = "Could not load from server!"
+
+function logout() {
+    sessionStorage.removeItem('token');
+}
+
+$(function () {
+    // sessionStorage.setItem('token', 'testing');
+    var token = sessionStorage.getItem('token');
+    if (token) {
+        if (window.location.pathname.split('/').slice(-1)[0]=='login.html') {
+            $(location).attr('href', 'index.html');
+        }
+        var nav_items = '<ul class="navbar-nav ml-auto">\
+                            <li class="nav-item">\
+                                <div class="dropdown show">\
+                                    <a class="btn btn-round btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+                                        <i class="material-icons">account_circle</i> Deepanjan\
+                                    </a>\
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" >\
+                                        <a class="dropdown-item" href="profile.html">Profile</a>\
+                                        <a class="dropdown-item" href="book_request.html">Book Request</a>\
+                                        <a class="dropdown-item" href="index.html" onClick="logout()">Logout</a>\
+                                    </div >\
+                                </div >\
+                            </li >\
+                        </ul>';
+        $("#navigation-items").html(nav_items);
+    } else {
+        var nav_items = '<ul class="navbar-nav ml-auto">\
+                            <li class="nav-item">\
+                                <a href="signup.html" class="nav-link">\
+                                    Signup\
+                                </a>\
+                            </li>\
+                            <li class="nav-item">\
+                                <a href="login.html" class="nav-link">\
+                                    Login\
+                                </a>\
+                            </li>\
+                        </ul>';
+        $("#navigation-items").html(nav_items);
+    }
+});
