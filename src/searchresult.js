e942fb292c54428553e09d1206d4a885ff809f9d
Deepanjan Datta <deepanjan052000@gmail.com>
2021-05-21 00:52:04 +0530

    Fixed empty query search glitch

--- src/searchresult.js
+++ src/searchresult.js
@@ -9,39 +9,45 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-var keyword = getParameterByName('keyword');
-var searchUrl = baseUrl + 'books/search/' + keyword;
-console.log("the URL", searchUrl);
-var books = [];
-// make an ajax call to the rest server to get the data
-$.ajax(searchUrl, {
-    method: 'GET',
-}).then(function (books) {
-    console.log(books); 
-    var book_out = "";
+var keyword = getParameterByName('keyword').trim();
 
-    books.TheListOfBooks.forEach(function (b) {
+if (keyword=="") {
+    var a = `<h2>No books found!!</h2>`;
+    $('#booksFound').html(a);
+} else {
+    var searchUrl = baseUrl + 'books/search/' + keyword;
+    console.log("the URL", searchUrl);
+    var books = [];
+    // make an ajax call to the rest server to get the data
+    $.ajax(searchUrl, {
+        method: 'GET',
+    }).then(function (books) {
+        console.log(books); 
+        var book_out = "";
 
-        book_out += '<div class="col-4 card">\
-                        <div class="card">\
-                            <img class="card-img-top" src=' + b.bookCover +' alt="Card image cap">\
-                            <div class="card-body">\
-                            <h5 class="card-title"> Title: ' + b.title +'</h5>\
-                            <h5 class="card-title"> Genre: ' + b.genre +'</h5>\
-                            <h5 class="card-info"> Author: ' + b.author +'</h5>\
-                            <p class="card-text">'+ parseFloat(b.rating).toFixed(1) +' /5.0</p>\
-                            <a href="#" class="btn btn-primary">Show More</a>\
+        books.TheListOfBooks.forEach(function (b) {
+
+            book_out += '<div class="col-4 card">\
+                            <div class="card">\
+                                <img class="card-img-top" src=' + b.bookCover +' alt="Card image cap">\
+                                <div class="card-body">\
+                                <h5 class="card-title"> Title: ' + b.title +'</h5>\
+                                <h5 class="card-title"> Genre: ' + b.genre +'</h5>\
+                                <h5 class="card-info"> Author: ' + b.author +'</h5>\
+                                <p class="card-text">'+ parseFloat(b.rating).toFixed(1) +' /5.0</p>\
+                                <a href="#" class="btn btn-primary">Show More</a>\
+                            </div>\
                         </div>\
-                    </div>\
-                    </div>';
+                        </div>';
 
+        });
+        $('#booksFound').html(book_out);
+        if(books.TheListOfBooks.length == 0){
+            var a = `<h2>No books found!!</h2>`;
+            $('#booksFound').html(a);
+        }
+    }).catch(function (err) {
+        console.error(err);
+        $('#all-genre').html(error_msg);
     });
-    $('#booksFound').html(book_out);
-    if(books.TheListOfBooks.length == 0){
-        var a = `<h2>No books found!!</h2>`;
-        $('#booksFound').html(a);
-    }
-}).catch(function (err) {
-    console.error(err);
-    $('#all-genre').html(error_msg);
-});
+}
