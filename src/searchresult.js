c5724e4fbdf3bbaab291b4392d52edfada540168
Kanav Ghai <41801620+ghaikanav@users.noreply.github.com>
2021-05-20 22:44:37 +0530

    Now works for any keyword

--- src/searchresult.js
+++ src/searchresult.js
@@ -1,4 +1,15 @@
-var keyword = "Biography";
+var url = window.location.href;
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var keyword = getParameterByName('keyword');
 var searchUrl = baseUrl + 'books/search/' + keyword;
 console.log("the URL", searchUrl);
 var books = [];
@@ -11,13 +22,13 @@ $.ajax(searchUrl, {
 
     books.TheListOfBooks.forEach(function (b) {
 
-        book_out += '<div class="col-md-3 col-xs-12">\
+        book_out += '<div class="col-4 card">\
                         <div class="card">\
                             <img class="card-img-top" src=' + b.bookCover +' alt="Card image cap">\
                             <div class="card-body">\
                             <h5 class="card-title"> Book Title' +b.title +'</h5>\
                             <h5 class="card-title"> Book Title' +b.genre +'</h5>\
-                            <p class="card-text">BOOK Description</p>\
+                            <p class="card-text">'+ parseFloat(b.rating).toFixed(1) +' /5</p>\
                             <a href="#" class="btn btn-primary">Show More</a>\
                         </div>\
                     </div>\
